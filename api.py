70e2e35ef6da3fc5f1a25afbce73f99a9ce046af
Rory Deken <deken.7@osu.edu>
2020-12-18 12:09:03 -0500

    updated todos and removed unnecessary packages

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import json
 
 import flask
 import requests
-from flask import escape, jsonify
+from flask import jsonify
 from flask_cors import CORS
 
 app = flask.Flask(__name__)
@@ -20,4 +20,24 @@ def get_players():
     return jsonify(json.loads(open("data.json", "r").read()))
 
 
+"""
+Routes
+     GET/
+        . GET Available players
+        . GET Drafted players/draft board
+        . GET Teams with current roster
+            * Team roster structure
+
+    POST/
+        . Add player to Team/remove from Available players
+        
+Admin things
+    . Ability to create an account/Team
+        * Different roles commisoner vs owner
+    . Create a league draft with time and number of teams
+        * Allow league settings, roster type (2WR/2RB etc), scoring type (PPR/Standard etc)
+        * Allow commisoner to pause/start and edit picks
+        * Allow snake format for draft
+        * Allow ability for teams to be forced to draft their starting lineup first
+"""
 app.run()
