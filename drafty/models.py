82fa23f50ca87ce9409257ae4070d552de52acef
deken.7 <deken.7@osu.edu>
2021-02-18 17:05:07 -0500

    new branch with project folder structure

---
+++ drafty/models.py
@@ -0,0 +1,28 @@
+from . import db
+
+
+class Player(db.Model):
+    __tablename__ = 'players'
+    id = db.Column(db.String(5), primary_key=True)
+    firstName = db.Column(db.String(200))
+    lastName = db.Column(db.String(200))
+    team = db.Column(db.String(5))
+    position = db.Column(db.String(3))
+    available = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return "Player(id='%s', firstName='%s', lastName='%s', team='%s', position='%s', available='%s')>" % (self.id, self.firstName, self.lastName, self.team, self.position, self.available)
+
+
+class Drafted(db.Model):
+    __tablename__ = 'drafted'
+    id = db.Column(db.String(5), primary_key=True)
+    firstName = db.Column(db.String(200))
+    lastName = db.Column(db.String(200))
+    team = db.Column(db.String(5))
+    position = db.Column(db.String(3))
+    round = db.Column(db.Integer())
+    ownedBy = db.Column(db.String(200))
+
+    def __repr__(self):
+        return "Player(id='%s', firstName='%s', lastName='%s', team='%s', position='%s', round='%s', ownedBy='%s')>" % (self.id, self.firstName, self.lastName, self.team, self.position, self.round, self.ownedBy)
