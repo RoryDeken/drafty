4311e6d359aa3f6b837789aaf082d513a36ac165
deken.7 <deken.7@osu.edu>
2021-02-24 13:27:07 -0500

    updated screipt

---
+++ drafty/import_players.py
@@ -0,0 +1,43 @@
+import click
+import requests
+import flask
+from flask import current_app as app
+from .models import db, Player, Drafted
+
+
+@app.cli.command('import')
+def importplayers():
+
+    print('Starting import...')
+    url = "https://api.sleeper.app/v1/players/nfl"
+
+    r = requests.get(url)
+    players_returned = flask.json.loads(r.text)
+    players = []
+    active_players = []
+
+    for player in players_returned:
+        players.append(players_returned[player])
+
+    for player in players:
+        if player["active"] == True and player["position"] == 'QB' or player["position"] == 'RB' or player["position"] == 'WR' or player["position"] == 'TE' or player["position"] == 'DEF' or player["position"] == 'K':
+            if 'status' in player.keys():
+                if player['status'] != 'Inactive':
+                    active_players.append(player)
+            else:
+                active_players.append(player)
+
+    players = sorted(active_players, key=lambda k: k['last_name'])
+
+    Player.query.delete()
+    Drafted.query.delete()
+    data = []
+    for player in players:
+        data.append(
+            Player(id=player['player_id'], firstName=player['first_name'], lastName=player['last_name'], team=str(
+                player['team']), position=player['position'], available=True)
+        )
+
+    db.session.add_all(data)
+    db.session.commit()
+    print('Import complete!!!')
